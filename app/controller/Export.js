a37c6f02aeac350449434c2ae9778096903c7a39
arieger <rieger@terrestris.de>
2016-06-16 11:16:55 +0200

    renamed controller classes according to angular semantic

--- app/controller/Export.js
+++ app/controller/Export.js
@@ -3,7 +3,7 @@
  */
 angular
     .module('SolrHeatmapApp')
-    .controller('Export', ['Map', '$scope', '$filter', '$timeout', function(MapService, $scope, $filter, $timeout) {
+    .controller('ExportCtrl', ['Map', '$scope', '$filter', '$timeout', function(MapService, $scope, $filter, $timeout) {
 
         $scope.startExport = function() {
           console.log("Here will be an awesome export");
